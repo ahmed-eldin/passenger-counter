6293f4e9ad0d959c4216d78c5c92c299521842a3
Ahmed Eldin <111728755+ahmed-eldin@users.noreply.github.com>
2023-04-11 18:16:24 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+console.log(count)
+let countDash = count + " - "
+saveEl.textContent += countDash
+countEl.textContent = 0
+count = 0
+}
+
+save()
